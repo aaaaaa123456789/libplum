91525b09111da6c7cf147bce7198ab87a9f2b3dc
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2021-12-07 21:16:00 -0300

    Remove unused error

--- src/misc.c
+++ src/misc.c
@@ -39,7 +39,6 @@ const char * plum_get_error_text (unsigned error) {
     [PLUM_OK]                      = "success",
     [PLUM_ERR_INVALID_ARGUMENTS]   = "invalid argument for function",
     [PLUM_ERR_INVALID_FILE_FORMAT] = "invalid image data or unknown format",
-    [PLUM_ERR_INVALID_IMAGE_DATA]  = "invalid image data",
     [PLUM_ERR_INVALID_METADATA]    = "invalid image metadata",
     [PLUM_ERR_INVALID_COLOR_INDEX] = "invalid palette index",
     [PLUM_ERR_TOO_MANY_COLORS]     = "too many colors in image",
