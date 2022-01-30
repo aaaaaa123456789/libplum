1cd6320ea3b0a5adbdddb2b74b28520b875e8b9b
aaaaaa123456789 <aaaaaa123456789@acidch.at>
2022-01-30 07:33:56 -0300

    fuzztest: always print startup time

--- fuzztest.c
+++ fuzztest.c
@@ -16,10 +16,8 @@ int main (int argc, char ** argv) {
       size += fread(data + size, 1, 0x4000, fp);
     }
     fclose(fp);
-    if (argc > 2) {
-      struct tm * timedata = localtime((const time_t []) {time(NULL)});
-      printf("[%02d:%02d:%02d] %s\n", timedata -> tm_hour, timedata -> tm_min, timedata -> tm_sec, *argv);
-    }
+    struct tm * timedata = localtime((const time_t []) {time(NULL)});
+    printf("[%02d:%02d:%02d] %s\n", timedata -> tm_hour, timedata -> tm_min, timedata -> tm_sec, *argv);
     LLVMFuzzerTestOneInput(data, size);
     free(data);
   }
